c6d91d6cb6ff87cf7ba09928f62d68f6846d9aca
Ivan <radeljicivan85@gmail.com>
2019-12-08 18:56:04 +0100

    Add port for backend app

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,11 @@ const app = express()
 
 app.use(helmet())
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/admin', admin())
+
+var port = process.env.PORT || 8080;
+app.listen(port)
+console.log('API server listening on port ' + port)
