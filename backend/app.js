87c6080672f0b217e53990b029cefb9b9f47fba2
Ivan <radeljicivan85@gmail.com>
2019-12-08 19:00:06 +0100

    Configure more middlewares for backend app

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const helmet = require('helmet')
-var admin = require('sriracha')
+const admin = require('sriracha')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(helmet())
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
+app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/admin', admin())
 
 var port = process.env.PORT || 8080;
-app.listen(port)
-console.log('API server listening on port ' + port)
+app.listen(port, () => {
+    console.log('API server listening on port ' + port)
+})
