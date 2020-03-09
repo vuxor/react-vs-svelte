23604c62470d87e656cb7d5eaae99b3e6efafd1d
Ivan <radeljicivan85@gmail.com>
2020-03-09 10:04:10 +0100

    Code format

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const helmet = require('helmet')
-const admin = require('sriracha')
-const morgan = require('morgan')
-require('dotenv').config()
+const express = require("express");
+const helmet = require("helmet");
+const admin = require("sriracha");
+const morgan = require("morgan");
+require("dotenv").config();
 
-require('./db.js')
-const users = require('./routes/api/v1.0/users')
+require("./db.js");
+const users = require("./routes/api/v1.0/users");
 
-const app = express()
+const app = express();
 
-app.use(helmet())
-app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
-app.use(morgan('combined'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(helmet());
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/admin', admin())
-app.use('/api/v1.0/users', users)
+app.use("/admin", admin());
+app.use("/api/v1.0/users", users);
 
 var port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log('API server listening on port ' + port)
-})
+  console.log("API server listening on port " + port);
+});
