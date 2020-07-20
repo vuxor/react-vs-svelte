10d1bbdf57c1b697bf6f395b84fb4b0c23013810
Ivan Radeljic <radeljicivan85@gmail.com>
2020-07-20 09:29:55 +0200

    Use local strategy in login

--- backend/routes/api/v1/users.js
+++ backend/routes/api/v1/users.js
@@ -2,37 +2,42 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const User = require("../../../models/user");
+const passport = require("passport");
+require("../../../strategies/localStrategy");
 
-router.get("/status", function(req, res) {
+router.get("/status", function (req, res) {
   res.json({ status: "User status" });
 });
 
-router.post("/login", function(req, res) {
-  const { username, password } = req.body;
-  res.json({ user: "User login" });
-  // use local strategy here
-});
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  function (req, res) {
+    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
+      expiresIn: 43200, // expires in 12 hours
+    });
+    res.status(200).send({ auth: true, token });
+  }
+);
 
-router.post("/register", function(req, res) {
+router.post("/register", function (req, res) {
   const { username, email, password } = req.body;
   User.create(
     {
       username,
       email,
-      password
+      password,
     },
-    function(err, user) {
+    function (err, user) {
       if (err) {
-        return res
-          .status(500)
-          .send({
-            message: "There was a problem registering the user."
-          });
+        return res.status(500).send({
+          register: false,
+          message: "There was a problem registering the user.",
+        });
       }
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: 43200 // expires in 12 hours
-      });
-      res.status(200).send({ auth: true, token });
+      res
+        .status(200)
+        .send({ register: true, message: "User is created", id: user._id });
     }
   );
 });
