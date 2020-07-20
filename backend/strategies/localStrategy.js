001f59ddb29fe9c7a73e79f741b08f91179c1ff8
Ivan Radeljic <radeljicivan85@gmail.com>
2020-07-20 09:21:06 +0200

    Add local strategy

---
+++ backend/strategies/localStrategy.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+
+passport.use(
+  new LocalStrategy(
+    {
+      session: false,
+    },
+    function (username, password, done) {
+      User.findOne({ username }, function (err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        user.comparePassword(password, function (error, isMatch) {
+          if (error) {
+            return done(err);
+          }
+          if (!isMatch) {
+            return done(null, false, { message: "Incorrect password" });
+          } else {
+            return done(null, user);
+          }
+        });
+      });
+    }
+  )
+);
