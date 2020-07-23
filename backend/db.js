d2173a0af7cc557c12b7dc5cffac067537c30d14
Ivan Radeljic <radeljicivan85@gmail.com>
2020-07-23 18:42:23 +0200

    Surpress mongoose warning

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ let mongoose = require("mongoose");
 mongoose.connect(process.env.DB_HOST, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true
 });
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
